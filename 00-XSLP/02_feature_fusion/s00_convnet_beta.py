ff5ed764fbc6e4774a0b2e17491f9bf2b82e0a50
Oliver <22160245.zju.edu.cn>
2022-12-15 16:25:16 +0800

    s00_convet_beta

--- 00-XSLP/02_feature_fusion/s00_convnet_beta.py
+++ 00-XSLP/02_feature_fusion/s00_convnet_beta.py
@@ -15,12 +15,12 @@ pass
 # -----------------------------------------------------------------------------
 # Specify summary file name and GPU ID here
 # -----------------------------------------------------------------------------
-summ_name = '1215_s00_data_fusion_convnet'
+summ_name = '1215_s00_feature_fusion_convnet'
 gpu_id = 0
 
 s.register('gather_summ_name', summ_name + '.sum')
 s.register('gpu_id', gpu_id)
-s.register('allow_growth', False)
+s.register('allow_growth', True)
 # -----------------------------------------------------------------------------
 # Set up your models and run
 # -----------------------------------------------------------------------------
@@ -28,9 +28,7 @@ s.register('train', True)
 s.register('epoch', 1000)
 s.register('patience', 15)
 
-# s.register('ratio', *[0.1*i for i in range(11)])
-data_configs = ['sleepedf:20:4','sleepedf:20:0,4',
-                'sleepedf:20:0,1,4','sleepedf:20:0,2,4']
+data_configs = ['sleepedf:20:0,2,4', 'sleepedf:20:0,1,2']
 s.register('data_config', *data_configs)
 test_configs = [f'test_data:{2*i},{2*i+1}' for i in range(10)]
 s.register('test_config', *test_configs)
